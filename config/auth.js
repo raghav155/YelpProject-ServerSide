99e7724a879d0566786c6794823837058854ddda
raghav <raghavg06@gmail.com>
2017-11-08 11:59:13 +0000

    nextcommit

--- config/auth.js
+++ config/auth.js
@@ -3,13 +3,13 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '2005932319624955', // your App ID
         'clientSecret'  : 'd9a2a45d572bebd60cf034ed17f40617', // your App Secret
-        'callbackURL'   : 'http://yelpproject.com:80/auth/facebook/callback'
+        'callbackURL'   : 'http://139.59.66.165.xip.io/auth/facebook/callback'
     },
 
     'googleAuth' : {
         'clientID'      : '724284366940-jbt5gelv9j22d3fnl6au4kl72uvpfo65.apps.googleusercontent.com',
         'clientSecret'  : 'qmUZSgvClPjBMwWD3FR19QA4',
-        'callbackURL'   : 'http://yelpproject.com/auth/google/callback'
+        'callbackURL'   : 'http://139.59.66.165.xip.io/auth/google/callback'
     }
 
 };
